ac0a3bfe96b3feaa0b048eee7296b4423ef6a6b9
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-06-25 12:00:21 +0545

    decorators example?

---
+++ example/decorators.ts
@@ -0,0 +1,21 @@
+function first() {
+    console.log("first(): factory evaluated");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("first(): called");
+    };
+}
+
+function second() {
+    console.log("second(): factory evaluated");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("second(): called");
+    };
+}
+
+class ExampleClass {
+    @first()
+    @second()
+    method() { }
+}
+
+new ExampleClass().method()
